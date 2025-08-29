f4a566f26d0fea69b8f2b2b0dfb015214f287256
nayoon030303 <kny030303khs@gmail.com>
2025-08-29 22:57:41 +0900

    sitemap 수정

--- router/sitemap.js
+++ router/sitemap.js
@@ -5,6 +5,10 @@ const config = require("../router/config");
 const Structure = require("../model/structure");
 const SearchSchema = require("../model/search");
 
+const JobVacancy = require("../model/jobVacancy");
+const OnlineTutor = require("../model/onlineTutor");
+const JobSeeker = require("../model/jobSeeker");
+
 /*
 키워드 검색 결과 화면
 */
@@ -19,36 +23,65 @@ router.post("", async (req, res, next) => {
     // console.log('searchText : ' + searchText);
     // console.log('----------------------------------------------');
 
-    // 검색어로 검색한 결과
-    let search_list = config.search.in.map((v) => {
-      let obj = {};
-      obj[v] = { $regex: searchText };
-      return obj;
-    });
+    const regex = new RegExp(searchText, "i");
 
-    let result = await Schema.aggregate([
-      { $match: { "@type": { $exists: true } } },
-      { $match: { $or: search_list } },
-      { $match: { "@type": { $nin: config.search.nin } } },
-      {
-        $group: {
-          _id: "$@type",
-          count: { $sum: 1 },
-          list: { $push: "$$ROOT" },
-        },
-      },
+    const [vacancies, tutors, seekers] = await Promise.all([
+      JobVacancy.find({
+        $or: [{ title: regex }, { description: regex }],
+      }).sort({ datePosted: -1 }),
+
+      OnlineTutor.find({
+        $or: [{ title: regex }, { description: regex }],
+      }).sort({ datePosted: -1 }),
+
+      JobSeeker.find({
+        $or: [{ title: regex }, { description: regex }],
+      }).sort({ datePosted: -1 }),
     ]);
 
-    // 데이터 정렬
-    result.sort((a, b) => {
-      if (a._id < b._id) return -1;
-      if (a._id > b._id) return 1;
-    });
+    const result = [
+      { _id: "Job_Vacancies", count: vacancies.length, list: vacancies },
+      { _id: "Online_Tutors", count: tutors.length, list: tutors },
+      { _id: "Job_Seekers", count: seekers.length, list: seekers },
+    ];
 
     res.render("sitemap", {
       result: result,
       searchText: searchText,
     });
+    return a;
+    // 검색어로 검색한 결과
+    // let search_list = config.search.in.map((v) => {
+    //   let obj = {};
+    //   obj[v] = { $regex: searchText };
+    //   return obj;
+    // });
+
+    // let result = await Schema.aggregate([
+    //   { $match: { "@type": { $exists: true } } },
+    //   { $match: { $or: search_list } },
+    //   { $match: { "@type": { $nin: config.search.nin } } },
+    //   {
+    //     $group: {
+    //       _id: "$@type",
+    //       count: { $sum: 1 },
+    //       list: { $push: "$$ROOT" },
+    //     },
+    //   },
+    // ]);
+
+    // // 데이터 정렬
+    // result.sort((a, b) => {
+    //   if (a._id < b._id) return -1;
+    //   if (a._id > b._id) return 1;
+    // });
+
+    // console.log("result", result);
+
+    // res.render("sitemap", {
+    //   result: result,
+    //   searchText: searchText,
+    // });
   } catch (e) {
     res.status(404);
   }
